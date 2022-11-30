d94664a43b1d992d815b49b454095af3ac1d24fd
Hugo Daniel <hugodaniel.icam@gmail.com>
2022-11-30 21:05:35 +0100

    Display routes on map

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -3,4 +3,11 @@ class Itinerary < ApplicationRecord
   has_many :routes
 
   validates :departure, :arrival, presence: true
+
+  def get_bounds
+    [
+      [ routes.first.departure_lng, routes.first.departure_lat ],
+      [ routes.first.arrival_lng, routes.first.arrival_lat ],
+    ]
+  end
 end
