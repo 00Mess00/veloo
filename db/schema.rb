e3692356a1b54959a9a4efe5d6a6aea06ab19901
Gaetan RENAULT <gadetan@gmail.com>
2022-12-02 14:26:24 +0100

    users sign-in

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_103939) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_105202) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -123,6 +122,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_103939) do
     t.string "skills"
     t.string "bike_type"
     t.float "average_speed"
+    t.string "visual_impairment"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
